4b169fe3205586cc85e60314b80d451ca423f11d
Mikael-R <mikarg9@gmail.com>
2020-08-29 15:35:30 -0300

    feat: 🎸 create command to generate password

--- src/commands/password-generator-cli.ts
+++ src/commands/password-generator-cli.ts
@@ -1,3 +1,5 @@
+import { generatePassword } from 'password-generator-package'
+
 import { GluegunCommand } from 'gluegun'
 import ExtendedGluegunToolbox from 'src/interfaces/extended-gluegun-toolbox'
 
@@ -5,10 +7,32 @@ const command: GluegunCommand = {
   name: 'password-generator-cli',
   run: (toolbox: ExtendedGluegunToolbox) => {
     const {
-      showBanner
+      showBanner,
+      print,
+      parameters: { options }
     } = toolbox
 
     showBanner({ text: 'Password Generator CLI' })
+
+    const preferences = {
+      length: options.length || options.l,
+      initialText: options.initialText || options.initTxt,
+      cases: {
+        uppercase: options.uppercase || options.upcs,
+        lowercase: options.lowercase || options.lwcs,
+        numbers: options.numbers || options.num,
+        symbols: options.symbols || options.sym
+      }
+    }
+
+    try {
+      const password = generatePassword(preferences)
+
+      print.success('Password generated with success!')
+      print.info(password)
+    } catch (error) {
+      print.error(error)
+    }
   }
 }
 
